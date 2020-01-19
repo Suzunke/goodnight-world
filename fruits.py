7657813e270f7bdb097252e584715a600c12c938
Suzunke <49409500+Suzunke@users.noreply.github.com>
2020-01-20 01:37:11 +0900

    すいか追加
    すいかを追加しました。

--- fruits.py
+++ fruits.py
@@ -1,8 +1,10 @@
 class fruits:
-    def __init__(self, apple, orange):
+    def __init__(self, apple, orange,watermelon):
         self.apple = apple
         self.orange = orange
+        self.watermelon = watermelon
 
-dic = fruits("ringo","mikan")
+dic = fruits("ringo","mikan","suika")
 print(dic.apple)
 print(dic.orange)
+print(dic.watermelon)
